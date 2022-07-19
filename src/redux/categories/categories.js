31cc6b6ec6ac84965bbd695e061b54bb7b822ad1
Farida-Hamid <faridaaddala@gmail.com>
2022-07-19 14:41:39 +0300

    Add reducer to catigories

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -5,3 +5,13 @@ const STATUS_CHECK = 'STATUS_CHECK';
 export const check = () => ({
   type: STATUS_CHECK,
 });
+
+// Reducer
+export default reducer = (state = [], action) => {
+  switch(action.type){
+    case STATUS_CHECK:
+      return "Under construction";
+    default:
+      return state;
+  }
+}
