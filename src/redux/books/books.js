4f7cb8e49d4d41a67aecf686936d1243545c256c
Farida-Hamid <faridaaddala@gmail.com>
2022-07-19 14:30:16 +0300

    Add reducer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -12,3 +12,16 @@ export const remove = (index) => ({
   type: REMOVE,
   payload: index
 });
+
+// Reducer
+export default reducer = (state = [], action) => {
+  switch(action.type){
+    case ADD:
+      return [...state, action.payload];
+    case REMOVE:
+      return [...state.slice(0, action.index),
+      ...state.slice(action.index + 1), state.length];
+    default:
+      return state;
+  }
+};
