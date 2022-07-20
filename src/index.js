c4c7569c66a916c8af3a2aed2736b1da651f8a70
Farida-Hamid <faridaaddala@gmail.com>
2022-07-20 09:23:25 +0300

    Connect React main component to Redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import rootStore from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Routes>
-      <App />
-    </Routes>
-  </React.StrictMode>,
+  <Provider store={rootStore}>
+    <React.StrictMode>
+      <Routes>
+        <App />
+      </Routes>
+    </React.StrictMode>
+  </Provider>,
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,10 @@
-import { configureStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
 const rootStore = configureStore({
-  book: booksReducer,
-  catigory: categoriesReducer,
+  reducer: booksReducer,
+  reducer2: categoriesReducer,
 });
 
 export default rootStore;
