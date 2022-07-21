e368981227d7e74874843c228e28bc8d203fb8a2
Farida-Hamid <faridaaddala@gmail.com>
2022-07-21 09:06:28 +0300

    Adjust index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import rootStore from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={rootStore}>
-    <React.StrictMode>
-      <Routes>
-        <App />
-      </Routes>
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={rootStore}>
+        <Router>
+          <App />
+        </Router>
+    </Provider>
+  </React.StrictMode>
 );
